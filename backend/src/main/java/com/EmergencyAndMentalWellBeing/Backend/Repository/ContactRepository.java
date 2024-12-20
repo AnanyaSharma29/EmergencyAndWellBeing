b4d4cd625cf2468f0db7b40e9ae1f94773608302
AnanyaSharma29 <96763584+AnanyaSharma29@users.noreply.github.com>
2024-12-20 21:44:27 +0530

    Delete backend directory

--- backend/src/main/java/com/EmergencyAndMentalWellBeing/Backend/Repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.EmergencyAndMentalWellBeing.Backend.Repository;
-
-import com.EmergencyAndMentalWellBeing.Backend.Model.Contact;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ContactRepository extends MongoRepository<Contact, String> {
-}
